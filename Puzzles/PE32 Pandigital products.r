6bad19f169b4240c99fc59f55ddf0a306355cd64
ChrisDembowski <james.dembowski@gmail.com>
2016-08-19 23:49:21 -0500

    Hard wrap comments

--- Puzzles/PE32 Pandigital products.r
+++ Puzzles/PE32 Pandigital products.r
@@ -3,13 +3,18 @@
 # Pandigital products
 # Problem 32
 
-# We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+# We shall say that an n-digit number is pandigital if it makes use of all the
+#   digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1
+#   through 5 pandigital.
 
-# The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+# The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing
+#   multiplicand, multiplier, and product is 1 through 9 pandigital.
 
-# Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+# Find the sum of all products whose multiplicand/multiplier/product identity
+#   can be written as a 1 through 9 pandigital.
 
-# HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+# HINT: Some products can be obtained in more than one way so be sure to only
+#   include it once in your sum.
 
 
 
@@ -20,11 +25,17 @@
 # First some math
 
 # Assuming we test every permutation of 123456789,
-#   ab * cde = fghi and cde * ab = fghi will both be possible to find, but will result in duplicate products. Therefore, we can restrict our search to cases in which the multicand is less than the multiplier. In other words, for x * y = z, we want x < y.
+#   ab * cde = fghi and cde * ab = fghi will both be possible to find, but will
+#   result in duplicate products. Therefore, we can restrict our search to cases
+#   in which the multicand is less than the multiplier. In other words, for
+#   x * y = z, we want x < y.
 
-# 9 * 999 < 10,000, the smallest five digit integer, so if we place our * after the first digit, the equals sign must go after the fifth digit.
+# 9 * 999 < 10,000, the smallest five digit integer, so if we place our * after
+#   the first digit, the equals sign must go after the fifth digit.
 
-# 11 * 1111 > 999, the largest three digit integer, and 99 * 99 < 10,000, so if we place our * after the second digit, the equals sign must go after the fifth digit.
+# 11 * 1111 > 999, the largest three digit integer, and 99 * 99 < 10,000, so if
+#   we place our * after the second digit, the equals sign must go after the
+#   fifth digit.
 
 # Thus, we can have a 1 or 2 digit multicand, but only a 4 digit product.
 
