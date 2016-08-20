52aa8cf3bd41f63fe379ece9f39ec79d67befd1a
ChrisDembowski <james.dembowski@gmail.com>
2016-08-19 23:28:45 -0500

    Clarify comments

--- Puzzles/Free Throws.r
+++ Puzzles/Free Throws.r
@@ -1,4 +1,6 @@
-# Bob has a probability of 0.3 to successfully make a free throw. What is the probability that Bob can make 5 free throws in a row in no more than 100 attempts?
+# Bob has a probability of 0.3 to successfully make a free throw. What is the
+#   probability that Bob can make 5 free throws in a row in no more than 100
+#   attempts?
 
 n = 100
 
@@ -7,9 +9,16 @@ success = 5
 p = 0.3
 
 
-# Model the problem as a Markov chain with six states. State 1 is a current run of 0, given that a run of five has not yet been achieved. State 2 is a current run of 1, given that a run of five has not yet been achieved. We continue similarly to State 5: a current run of 4, given that a run of five has not yet been achieved. The State 6 is that a run of five has been achieved. No other state is accessible from State 6.
+# # Model the problem as a Markov chain with six states. State 1 is a run of five
+#   has not yet been achieved and the current run is length 0. State 2 is a run
+#   of five has not yet been achieved and the current run is length 1. We
+#   continue similarly to State 5: a run of five has not yet been achieved and
+#   the current run is length 4. State 6 is that a run of five has been achieved.
+#   No other state is accessible from State 6.
 
-# First, build the row-stochastic Markov matrix. The entry in the ith row and jth column represents the probability of transitioning from the ith state to the jth state with a single free throw attempt.
+# First, build the row-stochastic Markov matrix. The entry in the ith row and jth
+#   column represents the probability of transitioning from the ith state to the
+#   jth state with a single free throw attempt.
 
 probMatrix = matrix( 0, ncol = success + 1, nrow = success + 1)
 
@@ -33,6 +42,8 @@ matrixPower = function( X, pow) { # pow should be integer > 1
 
 }
 
-# The 100-step transition matrix is found by raising probMatrix to the 100th power. The desired result, the probability of transitioning from the 1st state to the 6th state in 100 steps is found in the 1st row, and 6th column.
+# The 100-step transition matrix is found by raising probMatrix to the 100th
+#   power. The desired result, the probability of transitioning from the 1st
+#   state to the 6th state in 100 steps is found in the 1st row, and 6th column.
 
 matrixPower( probMatrix, n)[ 1, success + 1]
