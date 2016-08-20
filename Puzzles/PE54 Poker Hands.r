ad5ded7f40fdcdcc9a171050e97f92a3ae76fae2
ChrisDembowski <james.dembowski@gmail.com>
2016-08-20 00:13:47 -0500

    Hard wrap comments

--- Puzzles/PE54 Poker Hands.r
+++ Puzzles/PE54 Poker Hands.r
@@ -18,9 +18,20 @@
 # The cards are valued in the order:
 # 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace.
 
-# If two players have the same ranked hands then the rank made up of the highest value wins; for example, a pair of eights beats a pair of fives (see example 1 below). But if two ranks tie, for example, both players have a pair of queens, then highest cards in each hand are compared (see example 4 below); if the highest cards tie then the next highest cards are compared, and so on.
+# If two players have the same ranked hands then the rank made up of the highest
+#   value wins; for example, a pair of eights beats a pair of fives (see example
+#   1 below). But if two ranks tie, for example, both players have a pair of
+#   queens, then highest cards in each hand are compared (see example 4 below);
+#   if the highest cards tie then the next highest cards are compared, and so
+#   on.
 
-# The file, poker.txt <https://projecteuler.net/project/resources/p054_poker.txt>, contains one-thousand random hands dealt to two players. Each line of the file contains ten cards (separated by a single space): the first five are Player 1's cards and the last five are Player 2's cards. You can assume that all hands are valid (no invalid characters or repeated cards), each player's hand is in no specific order, and in each hand there is a clear winner.
+# The file, poker.txt
+#   <https://projecteuler.net/project/resources/p054_poker.txt>, contains
+#   one-thousand random hands dealt to two players. Each line of the file
+#   contains ten cards (separated by a single space): the first five are Player
+#   1's cards and the last five are Player 2's cards. You can assume that all
+#   hands are valid (no invalid characters or repeated cards), each player's
+#   hand is in no specific order, and in each hand there is a clear winner.
 
 # How many hands does Player 1 win?
 
@@ -215,11 +226,11 @@ bestHand = function( cards = CARDS) {
             
     }
     
-    tiebreaker = c( tiebreaker, c( 0, 0, 0, 0))
+    tiebreaker = c( tiebreaker, c( 0L, 0L, 0L, 0L))
         
     sum( c( value, tiebreaker)[1:6] * 16L ^ (5:0))
         # treat each element in c( value, tiebreaker) as successive digits in
-        #    6-digit hexidecimal integer (final value base 10 numeric) 
+        #   6-digit hexidecimal integer (final value numeric, base 10) 
     
 }
 
